e8a9a72e0659434ef6a56d6b364658325f03baf6
ofeore <ofeore@gmail.com>
2026-03-31 13:26:07 +0100

    Install recharts

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -1,1 +1,13 @@
 "use client";
+
+import { useState } from "react";
+
+export default function analytics() {
+  return (
+    <main className="appContainer">
+      <div>
+        <p>Analytics</p>
+      </div>
+    </main>
+  );
+}
