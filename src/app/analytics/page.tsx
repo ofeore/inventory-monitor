0551499c05e21ae39b1b710b12cf4fc97948a534
ofeore <ofeore@gmail.com>
2026-04-01 12:16:15 +0100

    Create three chart layout

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -20,45 +20,120 @@ const data = [
 
 export default function Analytics() {
   return (
-    <main className="appContainer">
-      <p>Analytics</p>
+    <main className="app-container">
+      <button></button>
+      <h2>Analytics</h2>
+      <div className="charts-container">
+        <div className="chart-card">
+          <h3 className="chart-title">Days until stockout</h3>
+          <ResponsiveContainer width="100%" height={300}>
+            <LineChart
+              data={data}
+              responsive
+              margin={{
+                top: 20,
+                right: 20,
+                bottom: 5,
+                left: 0,
+              }}
+            >
+              <CartesianGrid />
+              <Line
+                type="monotone"
+                dataKey="sales"
+                stroke="purple"
+                strokeWidth={2}
+                name="Daily sales"
+              />
+              <XAxis dataKey={"day"} />
+              <YAxis
+                dataKey={"sales"}
+                width="auto"
+                allowDecimals={false}
+                domain={[0, "dataMax"]}
+                label={{
+                  value: "Number of Sales",
+                  position: "insideLeft",
+                  angle: -90,
+                  textAnchor: "middle",
+                }}
+              />
+              <Legend align="right" />
+              <Tooltip />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
 
-      <ResponsiveContainer width="100%" height={300}>
-        <LineChart
-          data={data}
-          responsive
-          margin={{
-            top: 20,
-            right: 20,
-            bottom: 5,
-            left: 0,
-          }}
-        >
-          <CartesianGrid />
-          <Line
-            type="monotone"
-            dataKey="sales"
-            stroke="purple"
-            strokeWidth={2}
-            name="Daily sales"
-          />
-          <XAxis dataKey={"day"} />
-          <YAxis
-            dataKey={"sales"}
-            width="auto"
-            allowDecimals={false}
-            domain={[0, "dataMax"]}
-            label={{
-              value: "Number of Sales",
-              position: "insideLeft",
-              angle: -90,
-              textAnchor: "middle",
-            }}
-          />
-          <Legend align="right" />
-          <Tooltip />
-        </LineChart>
-      </ResponsiveContainer>
+        <div className="bottom-charts">
+          <div className="chart-card">
+            <h3 className="chart-title">Top seller</h3>
+            <ResponsiveContainer width="100%" height={300}>
+              <LineChart
+                data={data}
+                margin={{ top: 20, right: 20, bottom: 5, left: 0 }}
+              >
+                <CartesianGrid />
+                <Line
+                  type="monotone"
+                  dataKey="sales"
+                  stroke="purple"
+                  strokeWidth={2}
+                  name="Daily sales"
+                />
+                <XAxis dataKey="day" />
+                <YAxis
+                  dataKey="sales"
+                  width="auto"
+                  allowDecimals={false}
+                  domain={[0, "dataMax"]}
+                  label={{
+                    value: "Number of Sales",
+                    position: "insideLeft",
+                    angle: -90,
+                    textAnchor: "middle",
+                  }}
+                />
+                <Legend align="right" />
+                <Tooltip />
+              </LineChart>
+            </ResponsiveContainer>
+          </div>
+
+          <div className="chart-card">
+            <h3 className="chart-title">Most at risk product</h3>
+            <ResponsiveContainer width="100%" height={300}>
+              <LineChart
+                data={data}
+                margin={{ top: 20, right: 20, bottom: 5, left: 0 }}
+              >
+                <CartesianGrid />
+                <Line
+                  type="monotone"
+                  dataKey="sales"
+                  stroke="purple"
+                  strokeWidth={2}
+                  name="Daily sales"
+                />
+                <XAxis dataKey="day" />
+                <YAxis
+                  dataKey="sales"
+                  width="auto"
+                  allowDecimals={false}
+                  domain={[0, "dataMax"]}
+                  label={{
+                    value: "Number of Sales",
+                    position: "insideLeft",
+                    angle: -90,
+                    textAnchor: "middle",
+                  }}
+                />
+                <Legend align="right" />
+                <Tooltip />
+              </LineChart>
+            </ResponsiveContainer>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
